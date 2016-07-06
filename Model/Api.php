d48ea61f8c4543af4eec96fa34a8f11b880ee087
Sang Truong <thanhsangvnm@gmail.com>
2016-07-06 10:40:07 +0700

    Fixed sendgrid sending errors

--- Model/Api.php
+++ Model/Api.php
@@ -30,7 +30,20 @@ class Api
     {
         if ($this->_provider == Provider::PROVIDER_SENDGRID)
         {
-            return $this->_api->client->mail()->send()->post($message);
+            $from    = new \SendGrid\Email($message['from_name'], $message['from_email']);
+            $subject = $message['subject'];
+            $content = new \SendGrid\Content('text/plain', isset($message['text']) ? $message['text'] : '');
+            if (isset($message['html']))
+            {
+                $content = new \SendGrid\Content('text/html', $message['html']);
+            }
+            foreach ($message['to'] as $to)
+            {
+                $_to  = new \SendGrid\Email(null, $to['email']);
+                $mail = new \SendGrid\Mail($from, $subject, $_to, $content);
+                $this->_api->client->mail()->send()->post($mail);
+            }
+            return true;
         }
     }
 
